7408c25970a837bf995d83085e099cc7383ba7f2
miikke79 <74132243+miikke79@users.noreply.github.com>
2022-09-26 00:21:04 +0300

    in progress

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Alert} from 'react-native';
+import MapView, { Marker } from'react-native-maps';
+import React, { useState } from 'react';
+
 
 export default function App() {
+
+  const [address, setAddress] = useState('');
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  const fetchLocation = () => 
+  {  fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=K6UsLI0CHckInWVQwxQaQrdANObKx3hL&location=${address}`)  
+  .then(response => response.json())  
+  .then(data => {setLatitude(data.results[0].locations[0].latLng.lat), setLongitude(data.results[0].locations[0].latLng.lng)  })
+  .catch(error => {         Alert.alert('Error', error);   });
+} 
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        style={styles.map}   
+        region={{
+          latitude: {latitude},
+          longitude: {longitude},
+          latitudeDelta: 0.0322,
+          longitudeDelta: 0.0221,
+        }}
+      />
+      <TextInput
+      style={{fontSize: 18}}
+        placeholder='enter address'
+        onChangeText={text => setAddress(text)}
+      />
+      <View style={styles.button}>
+      <Button 
+    title='Show'
+    onPress={fetchLocation}
+    />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +52,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    alignSelf: 'stretch'
+  },
+  map: {
+    flex:1,
+    width: "100%",
+    height: "100%"
+  }
 });
